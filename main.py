54e5a6112ccf09db73fb72e3e6dd2038ab988b6f
Simon Haeni <simonbenjamin.haeni@students.bfh.ch>
2023-11-27 11:26:50 +0100

    Duplicate functions implemented

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from PIL import Image
-import imagehash
-import os
-import tkinter as tk
 from tkinter import filedialog, ttk
-
+from skimage import io, measure
+import imagehash
+import tkinter as tk
+import os
+import numpy as np
 
 class InputWindow:
     def __init__(self,window):
@@ -35,6 +36,25 @@ class InputWindow:
         # Variable zum speichern des ausgewählten Pfades
         self.selectFilePath = None
 
+        # Dropdown-Liste zur Auswahl der Duplikatserkennungsmethode
+        self.method_var = tk.StringVar()
+        self.method_var.set("Hash basierte Erkennung")
+        self.method_label = ttk.Label(window, text="Duplikatserkennungsmethode:")
+        self.method_label.pack(pady=10)
+        self.method_dropdown = ttk.Combobox(
+            window, textvariable=self.method_var,
+            values=["Hash basierte Erkennung", "Struktur basierte Erkennung", "Rotationsbasierte Erkennung"]
+        )
+        self.method_dropdown.pack(pady=10)
+
+        # Schwellenwert für Ähnlichkeit
+        self.threshold_var = tk.DoubleVar()
+        self.threshold_var.set(0.9)
+        self.threshold_label = ttk.Label(window, text="Schwellenwert für Ähnlichkeit:")
+        self.threshold_label.pack(pady=10)
+        self.threshold_entry = ttk.Entry(window, textvariable=self.threshold_var)
+        self.threshold_entry.pack(pady=10)
+
     def select_folder(self):
         file_path = filedialog.askdirectory()
         if file_path:
@@ -44,8 +64,8 @@ class InputWindow:
     def adopt_folder(self):
         if self.selectFilePath:
             self.window.destroy()
-            #Das OutputWindow wird geöffnet
-            OutputWindow(self.selectFilePath).run()
+            # Übergeben Sie method_var und threshold_var an die OutputWindow-Klasse
+            OutputWindow(self.selectFilePath, self.method_var, self.threshold_var).run()
 
 class DuplicateFinder:
     def __init__(self, path):
@@ -70,27 +90,83 @@ class DuplicateFinder:
                         print(f"Fehler beim Lesen von {filename}: {e}")
         return duplicates
     
-    #def funktion_2(self):
-    #def funktion_3(self):
-    #def funktion_4(self):
-    #def funktion_5(self):
+    def find_duplicates_structure(self, similarity_threshold):
+        images = {}
+        duplicates = []
+
+        for folder_name, subfolders, file_names in os.walk(self.path):
+            for filename in file_names:
+                supported_formats = (".png", ".jpg", ".jpeg", ".gif", ".bmp")
+                if filename.lower().endswith(supported_formats):
+                    filepath = os.path.join(folder_name, filename)
+                    try:
+                        img = io.imread(filepath)
+
+                        # Berechnung der strukturellen Ähnlichkeit
+                        for existing_img, existing_img_path in images.values():
+                            similarity = measure.compare_ssim(existing_img, img, multichannel=True)
+                            if similarity > similarity_threshold:
+                                duplicates.append((filename, os.path.basename(existing_img_path)))
+                                break
+                        else:
+                            # Falls keine Duplikate gefunden wurden, fügen Sie das Bild zur images-Datenstruktur hinzu
+                            images[filename] = (img, filepath)
+
+                    except Exception as e:
+                        print(f"Fehler beim Lesen von {filename}: {e}")
+        return duplicates
+
+    def find_duplicates_rotated(self):
+        hashes = {}
+        duplicates = []
+
+        for folder_name, subfolders, file_names in os.walk(self.path):
+            for filename in file_names:
+                supported_formats = (".png", ".jpg", ".jpeg", ".gif", ".bmp")
+                if filename.lower().endswith(supported_formats):
+                    filepath = os.path.join(folder_name, filename)
+                    try:
+                        with Image.open(filepath) as img:
+                            # Konvertieren Sie das Bild in Graustufen (L-Modus)
+                            img = img.convert("L")
+
+                            # Schleife durch verschiedene Drehungen des Bildes
+                            for angle in range(0, 360, 10):
+                                rotated_img = img.rotate(angle)
+                                h = imagehash.average_hash(rotated_img)
+
+                                if h in hashes:
+                                    duplicates.append((filename, os.path.basename(hashes[h])))
+                                    break  # Bei einem Duplikat die Schleife verlassen
+                                else:
+                                    hashes[h] = filepath
+                    except Exception as e:
+                        print(f"Fehler beim Lesen von {filename}: {e}")
+        return duplicates
 
 class OutputWindow:
-    def __init__(self, path):
+    def __init__(self, path, method_var, threshold_var):
         self.window = tk.Tk()
         self.window.title("Ergebnisse")
+        self.method_var = method_var  # Übergeben Sie method_var
+        self.threshold_var = threshold_var  # Übergeben Sie threshold_var
         self.show_duplicates_images(path)
 
     def show_duplicates_images(self, path):
-    # Klasse für die Duplikatsfindung wird aufgerufen => es werden verschiedene Funktionen angewendet
         finder = DuplicateFinder(path)
-        duplicates_hash = finder.find_duplicates_hash()
-        #duplicates_2= finder.funktion2()
-        #duplicates_3= finder.funktion3()
-        #duplicates_4= finder.funktion4()
-        #duplicates_5= finder.funktion5()
-        if duplicates_hash:
-            self.show_duplicates(duplicates_hash)
+        method = self.method_var.get()  # Zugriff auf method_var-Wert
+        threshold = self.threshold_var.get()  # Zugriff auf threshold_var-Wert
+        if method == "Hash basierte Erkennung":
+            duplicates = finder.find_duplicates_hash()
+        elif method == "Struktur basierte Erkennung":
+            duplicates = finder.find_duplicates_structure(threshold)  # Verwenden Sie threshold als Parameter
+        elif method == "Rotationsbasierte Erkennung":
+            duplicates = finder.find_duplicates_rotated()
+        else:
+            duplicates = []
+
+        if duplicates:
+            self.show_duplicates(duplicates)
         else:
             self.display_message("Keine Duplikate gefunden!")
 
@@ -99,7 +175,7 @@ class OutputWindow:
         label.pack(padx=10, pady=10)
 
     def show_duplicates(self, duplicates):
-        duplicate_list = tk.Listbox(self.window, width=50, height=20)
+        duplicate_list = tk.Listbox(self.window, width=150, height=20)
         duplicate_list.pack(padx=10, pady=10)
 
         for path1, path2 in duplicates:
