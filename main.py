f241d5ed42ae8762e6c24a92abff0d2c1e854ec4
Bandschapp <kusi@MacBook-Air-von-Markus>
2023-12-07 20:46:11 +0100

    dublikate mit Bildern im GUI angezeigt inkl. Checkbox zum Löschen der Datei

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from PIL import Image
-from tkinter import filedialog, ttk
+from PIL import Image, ImageTk #add ImageTk for feature/visual branch
+from tkinter import filedialog, ttk, filedialog, messagebox #add filedialog, messagebox for feature/visual branch
 from skimage import io, measure, color
 from skimage.metrics import structural_similarity
 from skimage.transform import resize
@@ -8,10 +8,13 @@ import tkinter as tk
 import os
 import numpy as np
 
+#############################################---INPUT WINDOW---################################################################
+
 class InputWindow:
  
    
     def __init__(self,window):
+        
         # Initialisieren von GUI-Komponenten
         self.window = window
         self.window.title("Duplikatserkennung")
@@ -32,8 +35,7 @@ class InputWindow:
         # Fensterposition einstellen, um es in der Mitte des Bildschirms zu platzieren
         self.window.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
-        # Ordner auswählen
-        
+        # Ordner auswählen        
         self.description = ttk.Label(
             window, text="Bitte wähle den Ordner mit den zu prüfenden Bildern:"
         )
@@ -102,7 +104,7 @@ class InputWindow:
             OutputWindow(self.selectFilePath, self.method_var.get(), self.threshold_var.get(), self.progress_bar).run()
 
 
- 
+ #############################################---DUBLICATE FINDER ---################################################################
 
 class DuplicateFinder:
     def __init__(self, path):
@@ -168,20 +170,96 @@ class DuplicateFinder:
         return duplicates
     
 
-
+#############################################---OUTPUT WINDOW---################################################################
 
 class OutputWindow:
     def __init__(self, path, method, threshold, progress_bar):
-        self.window = tk.Tk()
+        self.window = tk.Toplevel()  # Ändere zu Toplevel, um ein neues Fenster zu erstellen
         self.window.title("Ergebnisse")
+        self.path = path  # Definiere self.path
+        self.checkboxes = []  # Hinzufügen einer Liste zum Speichern von Checkbox-Referenzen
+        
+        # Erstelle Canvas und Frame für Scrollbar:
+        self.canvas = tk.Canvas(self.window, borderwidth=0, background="#ffffff")
+        self.frame = tk.Frame(self.canvas, background="#ffffff")
+        self.canvas.pack(side="left", fill="both", expand=True)
 
-        self.method = method  # Übergeben Sie method_var 
-        self.threshold = threshold  # Übergeben Sie threshold_var
+        # Scrollbar konfigurieren
+        self.vsb = tk.Scrollbar(self.window, orient="vertical", command=self.canvas.yview)
+        self.canvas.configure(yscrollcommand=self.vsb.set)
+        self.vsb.pack(side="right", fill="y")
+
+
+        # Speicher für Checkbox-Status und Bildpfade
+        self.checkbox_vars = []
+        self.image_paths = []
+
+
+        self.method = method  # Übergeben Sie method
+        self.threshold = threshold  # Übergeben Sie threshold
         self.progress_bar = progress_bar
+
+        # Erstelle und packe den Löschen-Button
+        self.delete_button = tk.Button(self.window, text="Ausgewählte löschen", command=self.delete_selected_images)
+        self.delete_button.pack()
+
+        # Füge den Frame zum Canvas hinzu
+        self.canvas_frame = self.canvas.create_window((0,0), window=self.frame, anchor="nw")
+
+        self.frame.bind("<Configure>", self._on_frame_resize)
         
         self.show_duplicates_images(path)
+    
+    def onFrameConfigure(self, event):
+            #Setzt die Scrollregion des Canvas um alle Inhalte einzuschließen.
+            self.canvas.configure(scrollregion=self.canvas.bbox("all"))
 
 
+    def _on_frame_resize(self, event=None):
+        # Aktualisiere die Scrollregion des Canvas auf die Größe des Frame
+        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
+
+    def add_image_with_checkbox(self, pair_frame, image_path):
+        
+        # Lade das Bild und füge es hinzu
+        img = Image.open(image_path)
+        img = img.resize((100, 100))  # Bildgröße anpassen
+        imgtk = ImageTk.PhotoImage(img)
+
+        # Erstelle und packe das Bild in den pair_frame
+        label = tk.Label(pair_frame, image=imgtk)
+        label.image = imgtk  # Referenz behalten
+        label.pack(side='left', padx=10)  # Hinzugefügter Abstand zwischen den Bildern
+
+
+        # Checkbox hinzufügen
+        var = tk.BooleanVar()
+        checkbox = tk.Checkbutton(pair_frame, text="Löschen", variable=var)
+        checkbox.pack(side='left')
+
+        # Dateinamen-Label hinzufügen
+        filename_label = tk.Label(pair_frame, text=os.path.basename(image_path))
+        filename_label.pack(side='left', padx=10)
+       
+        # Speichere den Status der Checkbox und den Bildpfad
+        self.checkbox_vars.append(var)
+        self.image_paths.append(image_path)
+        self.checkboxes.append(checkbox)  # Speichere die Referenz der Checkbox
+
+        
+
+    def delete_selected_images(self):
+
+        for i, (var, path) in enumerate(zip(self.checkbox_vars, self.image_paths)):
+            if var.get():  # Wenn die Checkbox ausgewählt ist
+                try:
+                    os.remove(path)  # Bild löschen
+                    print(f"Gelöscht: {path}")
+                    # Aktualisiere den Text der Checkbox auf "Gelöscht" und deaktiviere sie
+                    self.checkboxes[i].config(text="Gelöscht", state=tk.DISABLED)
+                except Exception as e:
+                    messagebox.showerror("Fehler", f"Fehler beim Löschen von {path}: {e}")
+    
     def show_duplicates_images(self, path):
         finder = DuplicateFinder(path)
         if self.method == "Hash basierte Erkennung":
@@ -191,8 +269,24 @@ class OutputWindow:
         else:
             duplicates = []
 
+        
+
         if duplicates:
-            self.show_duplicates(duplicates)
+            for path1, path2 in duplicates:
+                # Erstelle einen Frame für jedes Paar
+                pair_frame = tk.Frame(self.frame)
+                pair_frame.pack(side="top", fill="x", padx=10, pady=5)
+                                
+                full_path1 = os.path.join(self.path, path1)
+                full_path2 = os.path.join(self.path, path2)
+               
+                # Füge jedes Bild des Duplikat-Paars in einem horizontalen Frame hinzu
+                self.add_image_with_checkbox(pair_frame, full_path1)
+                self.add_image_with_checkbox(pair_frame, full_path2) 
+        
+
+        
+        
         else:
             self.display_message("Keine Duplikate gefunden!")
 
